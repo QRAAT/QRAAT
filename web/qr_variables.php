df44a0786ff835a285973f861ec3c5be4d0e5eb6
Marcel Losekoot <mlosekoot@ucdavis.edu>
2013-05-27 13:32:06 -0700

    Added web server files
    Index and a proto php status page.

---
+++ web/qr_variables.php
@@ -0,0 +1,8 @@
+<?php
+	global $path_to_tx_csv ;
+	global $path_to_sitelist_csv ;
+	global $path_to_status_log ;
+	$path_to_tx_csv = "/rmg_server/tx.csv" ;
+	$path_to_sitelist_csv = "/rmg_server/sitelist.csv" ;
+	$path_to_status_log = "/rmg_server/status.log" ;
+?>
