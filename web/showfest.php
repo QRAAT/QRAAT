fcaddee87cca915c0cc72acd56250fd5e6c79b22
Marcel Losekoot <mlosekoot@ucdavis.edu>
2013-07-20 21:56:18 -0700

    Added user selectable time interval to showfest.php

--- web/showfest.php
+++ web/showfest.php
@@ -26,24 +26,82 @@ table.status td {
 </style>
 </head>
 <body>
+<p><a href='status.phtml'>Back</a></p>
 <?php
-	print "<p><a href='status.phtml'>Back</a></p>\n" ;
+	$input_duration_1hour = "unchecked" ;
+	$input_duration_2hours = "unchecked" ;
+	$input_duration_6hours = "unchecked" ;
+	$input_duration_1day = "unchecked" ;
+	$input_duration_2days = "unchecked" ;
+	$interval = 6*3600 ;
+#	if( isset($_POST['radio_duration']) )
+#	{
+#		print "<p>debug: POST['radio_duration']=".$_POST['radio_duration']."</p>\n" ;
+#	}
+	if( isset($_POST['radio_duration']) )
+	{
+		$radio_duration = $_POST['radio_duration'] ;
+		#print "<p>debug: radio_duration='$radio_duration'</p>\n" ;
+		if( $radio_duration == "1hour" )
+		{
+			$input_duration_1hour = "checked" ;
+			$interval = 3600 ;
+		}
+		if( $radio_duration == "2hours" )
+		{
+			$input_duration_2hours = "checked" ;
+			$interval = 2*3600 ;
+		}
+		if( $radio_duration == "6hours" )
+		{
+			$input_duration_6hours = "checked" ;
+			$interval = 6*3600 ;
+		}
+		if( $radio_duration == "1day" )
+		{
+			$input_duration_1day = "checked" ;
+			$interval = 24*3600 ;
+		}
+		if( $radio_duration == "2days" )
+		{
+			$input_duration_2days = "checked" ;
+			$interval = 48*3600 ;
+		}
+	}
+	else
+	{
+		$input_duration_6hours = "checked" ;
+		$interval = 6*3600 ;
+	}
+?>
+<form name="form_showfest" method="post" action="showfest.php">
+	<input type="radio" name="radio_duration" value="1hour" onclick="this.form.submit();" <?php print $input_duration_1hour; ?>>1 Hour
+	<input type="radio" name="radio_duration" value="2hours" onclick="this.form.submit();" <?php print $input_duration_2hours; ?>>2 Hours
+	<input type="radio" name="radio_duration" value="6hours" onclick="this.form.submit();" <?php print $input_duration_6hours; ?>>6 Hours
+	<input type="radio" name="radio_duration" value="1day" onclick="this.form.submit();" <?php print $input_duration_1day; ?>>1 Day
+	<input type="radio" name="radio_duration" value="2days" onclick="this.form.submit();" <?php print $input_duration_2days; ?>>2 Days
+</form>
+<?php
 	include($_SERVER['DOCUMENT_ROOT']."/dbcredentials.php") ;
-	$data = getdata("select count(id),min(datetime),max(datetime) from fest where datetime > date_add(now(), interval -1 day)") ;
+	if( $interval < 1 )
+	{
+		$interval = 6*3600 ; # 6hours
+	}
+	$query = "select count(id),min(datetime),max(datetime) from fest where datetime > date_add(utc_timestamp(),interval -$interval second)" ;
+	$data = getdata($query) ;
 	$count = $data[0][0] ;
 	$dtstart = $data[0][1] ;
 	$dtstop = $data[0][2] ;
 	if( $count > 0 )
 	{
 		print "Showing {$data[0][0]} entries between {$data[0][1]} and {$data[0][2]} UTC<br />\n" ;
-		$data = getdata("select frequency,datetime,name,fdsp,fdsnr,band3,band10 from fest,sitelist where sitelist.id = fest.siteid and datetime > date_add(now(), interval -1 day) order by frequency,datetime,name") ;
+		$data = getdata("select frequency,datetime,name,fdsp,fdsnr,band3,band10 from fest,sitelist where sitelist.id = fest.siteid and datetime > date_add(utc_timestamp(), interval -$interval second) order by frequency,datetime,name") ;
 		showdata(array("Frequency","Datetime (UTC)","Site","Power","SNR","Band3dB","Band10dB"),$data) ;
 	}
 	else
 	{
 		print "No data.\n" ;
 	}
-	print "</body>\n</html>\n" ;
 
 	function getdata($query)
 	{
