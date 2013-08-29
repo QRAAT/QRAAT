efaaf276807ab419fea06e9d4560216f1cc77e63
Chris <cjpatton@ucdavis.edu>
2013-08-29 12:12:42 -0700

    Added unit test for pulse detector.

--- python/rmg/qa_rmg.py
+++ python/rmg/qa_rmg.py
@@ -20,8 +20,13 @@
 # Boston, MA 02111-1307, USA.
 # 
 
+#editted by Todd Borrowman ECE-UIUC 01/18/08~02/2010
+
 from gnuradio import gr, gr_unittest, blks2
-import rmg
+import qraat.rmg, sys
+
+tx_name = "test"
+test_data = "unit_test.tdat"
 
 class qa_rmg (gr_unittest.TestCase):
 
@@ -32,27 +37,23 @@ class qa_rmg (gr_unittest.TestCase):
         self.fg = None
 
     def test_001_detect (self):
-        
-	src_file = gr.file_source(gr.sizeof_gr_complex, "./testdata.tdat")
-	#src_file = gr.file_source(gr.sizeof_gr_complex, "../../../uiuc_tracker/data_backup20070423/20070208164806.tdat")
-        #src_file = gr.file_source(gr.sizeof_gr_complex, "../../../uiuc_tracker/20080911161545.tdat")
+	src_file = gr.file_source(gr.sizeof_gr_complex, test_data)
+	#src_file = gr.file_source(gr.sizeof_gr_complex, "data_backup20070423/20070208164806.tdat")
+        #src_file = gr.file_source(gr.sizeof_gr_complex, "20080911161545.tdat")
         di = gr.deinterleave(gr.sizeof_gr_complex)
 		
-	 	
         self.fg.connect(src_file, di)
-        pd = rmg.detect(160,160,4,"./detect_test_results/",8000,0,0)
+        pd = qraat.rmg.detect(160,480,4, "results", tx_name, 8000, 0, 0)
 	pd.rise_factor(1.5)
 	pd.fall_factor(1.3)
+        pd.alpha_factor(0.0001)
+        pd.enable()
         self.fg.connect((di,0),(pd,0))
         self.fg.connect((di,1),(pd,1))
         self.fg.connect((di,2),(pd,2))
         self.fg.connect((di,3),(pd,3))
         self.fg.run();
         self.assertFloatTuplesAlmostEqual ([1],[1],1)
-
-
-
-
 
 
 if __name__ == '__main__':
