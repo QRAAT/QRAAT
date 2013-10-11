3f6811fcb013fa1caa3d7add950696e127de91cb
Chris <cjpatton@ucdavis.edu>
2013-10-11 16:51:23 -0700

    Moved gps_data class into API.

--- python/__init__.py
+++ python/__init__.py
@@ -1,24 +1,24 @@
 #
-# Copyright 2008,2009 Free Software Foundation, Inc.
-#
-# This application is free software; you can redistribute it and/or modify
+# Copyright (C) 2013 Todd Borrowman, Christopher Patton
+# 
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-#
-# This application is distributed in the hope that it will be useful,
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2008,2009 Free Software Foundation, Inc.
 #
 
 '''
-This is the Python ``qraat`` module, comprising our application programming
-interface. *This doc string is in python/__init__.py*. 
+  This is the Python ``qraat`` module, comprising our application programming
+  interface. *This doc string is in python/__init__.py*. 
 '''
 
 def enum(*sequential, **named):
@@ -45,6 +45,6 @@ def pretty_printer(val):
     return str(val)
 
 from csv import * 
+from gps import gps
 from det import det
 from est import est, ResolveIdError
-
