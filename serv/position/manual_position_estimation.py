1c572a88be39e7254ed8e269c8879dc153f863be
Chris <cjpatton@ucdavis.edu>
2013-10-28 12:01:58 -0700

    Working through manual_position_estimation ...

--- serv/position/manual_position_estimation.py
+++ serv/position/manual_position_estimation.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as pp
 import MySQLdb as mdb
 import numpy as np
 import time, os, sys
@@ -9,7 +10,8 @@ start_time_str = "201310140800"
 stop_time_str =  "201310141400"
 start_time = time.mktime(time.strptime(start_time_str,'%Y%m%d%H%M%S'))
 stop_time = time.mktime(time.strptime(stop_time_str,'%Y%m%d%H%M%S'))
-
+start_time = 1376420400.0
+stop_time =  1376442000.0
 # Get database credentials. 
 try: 
   db_config = qraat.csv("%s/db_auth" % os.environ['RMG_SERVER_DIR']).get(view='reader')
@@ -31,65 +33,49 @@ cur = db_con.cursor()
 
 #get site locations
 cur.execute("SELECT ID, easting, northing from sitelist;")
-site_data = np.array(cur.fetchall(),dtype=float)
+sites = qraat.csv(db_con=db_con, db_table='sitelist')
+print sites
 
 #get steering vector data
 sv_siteID = []
 sv = []
 sv_bearing = []
 
-for j in site_data[:,0]:
-  print "Getting Steering Vectors for Site ID #{}".format(j)
+for site in sites:
+  print "Getting Steering Vectors for Site ID #{}".format(site.ID)
   cur.execute('''SELECT Bearing, 
                         sv1r, sv1i, sv2r, sv2i, 
                         sv3r, sv3i, sv4r, sv4i 
                    FROM Steering_Vectors 
-                  WHERE SiteID=%s and Cal_InfoID=%s''', (j, cal_id))
+                  WHERE SiteID=%s and Cal_InfoID=%s''', (site.ID, cal_id))
   sv_data = np.array(cur.fetchall(),dtype=float)
   if sv_data.shape[0] > 0:
-    sv_siteID.append(j)
+    sv_siteID.append(site.ID)
     sv_bearing.append(np.array(sv_data[:,0]))
     sv.append(np.array(sv_data[:,1::2]+np.complex(0,1)*sv_data[:,2::2]))
 
 #get pulse groups
 print "Getting EST data from {0} to {1}".format(start_time_str, stop_time_str)
-cur.execute('''SELECT ID, siteid, timestamp, 
-                      ed1r, ed1i, ed2r, ed2i, 
-                      ed3r, ed3i, ed4r, ed4i 
-                 FROM est 
+cur.execute('''SELECT ID, siteid, timestamp,
+                      ed1r, ed1i, ed2r, ed2i,
+                      ed3r, ed3i, ed4r, ed4i
+                 FROM est
                 WHERE timestamp >= %s 
-                  AND timestamp <= %s''',(start_time, stop_time))
+                  AND timestamp <= %s''', (start_time, stop_time))
 signal_data = np.array(cur.fetchall(),dtype=float)
 print "position: processing %d records" % signal_data.shape[0]
 # TODO if signal_data.shape[0] == 0, exit
-sig_id = signal_data[:,0]
-site_id = signal_data[:,1]
-est_time = signal_data[:,2]
-signal = signal_data[:,3::2]+np.complex(0,-1)*signal_data[:,4::2]
+if signal_data.shape[0] == 0:
+  print >>sys.stderr, "position_est: fatal: no est records for selected time range."
+  sys.exit(1)
+
+sig_id =     np.array(signal_data[:,0], dtype=int)
+site_id =    np.array(signal_data[:,1], dtype=int)
+est_time =   signal_data[:,2]
+signal =     signal_data[:,3::2]+np.complex(0,-1)*signal_data[:,4::2]
 sort_index = np.argsort(est_time)
 
-if False:
-  pulse_groups = []
-  group_window = .02
-  time_index=0
-  while time_index < sort_index.shape[0]:
-    t = est_time[sort_index[time_index]]
-    temp_pulse = [ sort_index[time_index], ]
-    group_index = time_index + 1
-    while group_index < sort_index.shape[0] and est_time[sort_index[group_index]] < (t + group_window):
-      temp_pulse.append(sort_index[group_index])
-      group_index += 1
-    time_index = group_index
-    pulse_groups.append(temp_pulse)
-
-#get position data
-print "Getting Position data"
-cur.execute("select estID, easting, northing, bearing from True_Position")
-position_data = np.array(cur.fetchall(),dtype=float)
-
-
-
-
+# Calculate bearing likelihood per est record
 likelihoods = np.zeros((len(sort_index),360))
 bearings = []
 for iter_index in range(len(sort_index)):
@@ -102,14 +88,18 @@ for iter_index in range(len(sort_index)):
   for index, value in enumerate(sv_bearing[sv_index]):
     likelihoods[iter_index,value] = bearing_likelihood[0,index]
 
-site_pos = np.zeros((site_data.shape[0],),np.complex)
+# "Format location of sites"
+site_pos = np.zeros((len(sites),),np.complex)
 site_pos_id = []
-for site_index in range(site_data.shape[0]):
-  site_pos[site_index] = np.complex(site_data[site_index,2],site_data[site_index,1])
-  site_pos_id.append(site_data[site_index,0])
+for j in range(len(sites)):
+  site_pos[j] = np.complex(sites[j].northing, sites[j].easting)
+  site_pos_id.append(sites[j].ID)
+
+# Positions are stored as complex(northing, eassting).
 
 #center = 4251500+j*574500, step_size = 100
 def get_grid(center, step_size, half_span=15):
+  # Return a 2D array of positions. 
   position_grid = np.zeros((half_span*2+1, half_span*2+1),np.complex)
   for east_index in range(-half_span,half_span+1):
     for north_index in range(-half_span,half_span+1):
@@ -117,12 +107,15 @@ def get_grid(center, step_size, half_span=15):
   return position_grid
 
 def get_site_bearings(position_grid):
+  # Return a 3D array of bearings (in degree) over the position 
+  # grid, with the 3rd dimension is over different sites
   site_bearings = np.zeros(np.hstack((position_grid.shape,len(sv_siteID))))
   for sv_index, id_index in enumerate(sv_siteID):
     site_bearings[:,:,sv_index] = np.angle(position_grid - site_pos[site_pos_id.index(id_index)])*180/np.pi
   return site_bearings
 
 def get_pos_likelihood(index_list, site_bearings):
+  # Return a 2D array of likelihoods
   pos_likelihood = np.zeros(site_bearings.shape[0:2])
   for est_index in index_list:
     sv_index = sv_siteID.index(site_id[est_index])
@@ -130,6 +123,7 @@ def get_pos_likelihood(index_list, site_bearings):
   return pos_likelihood
 
 def position_estimation(index_list):
+  # Iteratively finds the position of maximum likelihood
   grid_center = np.complex(4260500,574500)
   step_size = 100
   count = 0
@@ -142,6 +136,8 @@ def position_estimation(index_list):
     count += 1
   return grid_center
 
+# Calculate position estimate in a 30 second window centered
+# around each est. Store in 1D array indexed by time. 
 avg_win = 30
 pos_est = np.zeros(sort_index.shape, dtype = np.complex)
 index=0
@@ -154,3 +150,7 @@ while index < sort_index.shape[0]:
   pos_est[sort_index[index]] = position_estimation(index_list)
   index += 1
 
+print pos_est
+
+pp.plot(np.imag(pos_est), np.real(pos_est), '.')
+pp.show()
