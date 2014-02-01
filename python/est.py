Need to fix bearing probability calculation.
Write class bearing on Monday.

python/est.py
@@ -367,8 +367,8 @@ class est2:
     # Store eigenvalue decomposition vectors and noise covariance
     # matrices in NumPy arrays. 
     cur = db_con.cursor()
-    cur.execute('''SELECT ID, siteid, txid, timestamp,
-                          edsp, ed1r, ed1i, ed2r, ed2i, ed3r, ed3i, ed4r, ed4i, 
+    cur.execute('''SELECT ID, siteid, txid, timestamp, edsp, 
+                          ed1r,  ed1i,  ed2r,  ed2i,  ed3r,  ed3i,  ed4r,  ed4i, 
                           nc11r, nc11i, nc12r, nc12i, nc13r, nc13i, nc14r, nc14i, 
                           nc21r, nc21i, nc22r, nc22i, nc23r, nc23i, nc24r, nc24i, 
                           nc31r, nc31i, nc32r, nc32i, nc33r, nc33i, nc34r, nc34i, 
@@ -385,11 +385,11 @@ class est2:
      self.site_id, 
      self.tx_id) = (np.array(raw[:,i], dtype=int) for i in range(0,3))
     self.timestamp = raw[:,3]
-    raw = raw[:,5:]
+    raw = raw[:,4:]
 
     # Signal power. 
     self.edsp = raw[:,0]
-    raw = raw[:,0:]
+    raw = raw[:,1:]
 
     # Signal vector, N x 1.
     self.ed = raw[:,0:8:2] + np.complex(0,-1) * raw[:,1:8:2]
@@ -406,13 +406,31 @@ if __name__=="__main__":
 
   try:
     db_con = mdb.connect('localhost', 'root', 'woodland', 'qraat')
-    fella = est2(db_con, 1376420800.0, 1376427800.0)
-    print fella.edsp
 
     guy = est()
     guy.read_db(db_con, 1376420800.0, 1376427800.0)
-    for i in range(len(guy)):
-      print guy[i].edsp
+    print np.array( [
+      [ np.complex(guy[23].nc11r, guy[23].nc11i), 
+        np.complex(guy[23].nc12r, guy[23].nc12i), 
+        np.complex(guy[23].nc13r, guy[23].nc13i), 
+        np.complex(guy[23].nc14r, guy[23].nc14i) ],
+      [ np.complex(guy[23].nc21r, guy[23].nc21i), 
+        np.complex(guy[23].nc22r, guy[23].nc22i), 
+        np.complex(guy[23].nc23r, guy[23].nc23i), 
+        np.complex(guy[23].nc24r, guy[23].nc24i) ],
+      [ np.complex(guy[23].nc31r, guy[23].nc31i), 
+        np.complex(guy[23].nc32r, guy[23].nc32i), 
+        np.complex(guy[23].nc33r, guy[23].nc33i), 
+        np.complex(guy[23].nc34r, guy[23].nc34i) ],
+      [ np.complex(guy[23].nc41r, guy[23].nc41i), 
+        np.complex(guy[23].nc42r, guy[23].nc42i), 
+        np.complex(guy[23].nc43r, guy[23].nc43i), 
+        np.complex(guy[23].nc44r, guy[23].nc44i) ],
+        ] )
+
+    fella = est2(db_con, 1376420800.0, 1376427800.0)
+    print fella.nc[23]
+
 
   except mdb.Error, e:
     print sys.stderr, "error (%d): %s" % (e.args[0], e.args[1])

serv/pos/plot_bearing.py
@@ -23,6 +23,11 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# TODO 
+# - Probability calculatio is wrong ... gotta figure this out. 
+# - Write class bearing, which will replace bearing_likelihoods. 
+
 
 import matplotlib.pyplot as pp
 from mpl_toolkits.mplot3d import Axes3D
@@ -67,12 +72,9 @@ parser.add_option('--t-end', type='float', metavar='SEC', default=1376432160,#13
 
 (options, args) = parser.parse_args()
 
-def plot_distribution(bl, i):
+def plot_distribution(p, ll, t, s):
 
   fig = pp.gcf()
-
-  s = bl.site_id[i]
-  ll = bl.likelihoods[i,] 
 
   indexMax = np.argmax(ll) 
   x = map(lambda x0 : x0 % 360, 
@@ -82,8 +84,10 @@ def plot_distribution(bl, i):
   ax = fig.add_subplot(1,1,1)
   ax.axis([0, 360, 0, ll[indexMax] + (0.15 * ll[indexMax])])
 
-  ax.fill_between(range(0,360), [ll[x0] for x0 in x], 0, color='b', 
+  ax.fill_between(range(0,360), [ll[x0] for x0 in x], 0, color='b',
     alpha='0.20', label='Data window')
+
+  ax.plot(range(0,360), [p[x0] for x0 in x], color='g')
 
   ax.plot([180, 180], [0, ll[indexMax]], '-', color='0.30', 
     label='Max likelihood')
@@ -104,7 +108,6 @@ def plot_distribution(bl, i):
   pp.xlabel("Bearing to SiteID=%d" % s)
   pp.ylabel("Likelihood")
 
-  t = time.localtime(bl.est_time[i])
   pp.title('%04d-%02d-%02d %02d%02d:%02d txID=%d' % (
      t.tm_year, t.tm_mon, t.tm_mday,
      t.tm_hour, t.tm_min, t.tm_sec,
@@ -127,12 +130,13 @@ bl.est_ids = bl.sig_id = data.id
 bl.site_id = data.site_id
 bl.est_time = data.timestamp
 bl.signal  = data.ed
-#bl.calc_likelihoods()
+bl.calc_likelihoods()
 
 
 def calc_prob_distribution(bl, est, t):
   
-  P = {}
+  p  = np.zeros(360)
+  ll = np.zeros(360)
 
   # Noise covariance matrix.
   Sigma = est.nc[t]
@@ -157,15 +161,19 @@ def calc_prob_distribution(bl, est, t):
 
     a = np.dot(np.dot(np.conj(np.transpose(V)), np.linalg.inv(R)), V) 
           
-    P[theta] = np.exp(-1 * a) / (b * np.linalg.det(R))
+    p[theta] = np.exp(-1 * a.real) / (b * np.linalg.det(R).real)
+    
+    left_half = np.dot(V, np.conj(np.transpose(G)))
+    ll[theta] = (left_half * np.conj(left_half)).real
 
-  print "P(theta) range:", min(P.values()), max(P.values())
+  print "P(theta) range:", min(p), max(p)
+  return (p + 0.5, ll)
   
 
 
-
-
-calc_prob_distribution(bl, data, 0)
-  
-
+i = 0
+(p, ll) = calc_prob_distribution(bl, data, i) 
+plot_distribution( p, ll,     
+                   time.localtime(bl.est_time[i]), 
+                   bl.site_id[i] )
 