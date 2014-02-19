06525e0f3b8666f17eb9914bd7468282df58fdf9
Christopher Patton <cjpatton@ucdavis.edu>
2014-02-18 19:52:51 -0800

    Finished critical path, I can't believe it works.
    TODOs in python/tracks.py.

--- python/tracks.py
+++ python/tracks.py
@@ -14,6 +14,13 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# TODOs 
+#  - Stackify DFS in toposort(). 
+#  - Fix mean / stddev calculation of target speed. 
+#  - Clean up class track, class Node. 
+#  - Interface for class track, include in __init__.py. 
+
 
 import numpy as np
 import time, os, sys
@@ -26,43 +33,42 @@ try:
   import MySQLdb as mdb
 except ImportError: pass
 
+
 def dist(Pi, Pj): 
   ''' Euclidean distance between points Pi and Pj. ''' 
   return np.sqrt((Pi.real - Pj.real)**2 + (Pi.imag - Pj.imag)**2)
 
-
 class Node: 
 
   def __init__(self, P, t, ll): 
-    self.c_parent = None
-    self.c_size   = 1
-    self.c_height = 0
+    # Position.  
     self.P = P
     self.t = t
     self.ll = ll
-    self.visited = False
-    self.adj = []
-    self.children = []
 
-  def dfs(self):
-    S = [self]; 
-    ct = 0
-    while (len(S) != 0): 
-      u = S.pop()
-      if not u.visited: 
-        u.visited = True
-        ct += 1
-        for v in u.adj:
-          S.append(v)
-    return ct
+    # Connected component analysis. 
+    self.c_size   = 1
+    self.c_height = 0
+  
+    # Topological sorting. 
+    self.t_visited = False
+    self.t_sorted = False
+
+    # Critical path. 
+    self.distance = 0
+
+    # Generic. 
+    self.parent = None
+    self.adj_in = []
+    self.adj_out = []
 
   def dist (self, aNode):
     return dist(self.P, aNode.P)
 
   def c_find(self):
     p = self
-    while (p.c_parent != None): 
-      p = p.c_parent
+    while (p.parent != None): 
+      p = p.parent
     return p
 
   def c_union(self, u): 
@@ -73,95 +79,28 @@ class Node:
       p = x
     
     elif (x.c_height == y.c_height): # x and y have the same height.
-      y.c_parent  = x
+      y.parent  = x
       x.c_height += 1
       x.c_size   += y.c_size
       p = x
 
     elif (x.c_height > y.c_height): # x is taller than y.
-      y.c_parent = x
+      y.parent = x
       x.c_size  += y.c_size
       p = x
 
     else: # y is taller than x.
-      x.c_parent = y
+      x.parent = y
       y.c_size  += x.c_size
       p = y
       
     return p
 
 
+
 class track:
+
   ''' Track.
-
-  :param max_speed: Maximum foot speed of target (m/s). 
-  :type max_speed: float
-  '''
-
-  def __init__(self, db_con, t_start, t_end, tx_id, max_speed):
-    cur = db_con.cursor()
-    cur.execute('''SELECT ID, txID, timestamp, northing, easting, likelihood
-                     FROM Position
-                    WHERE (%f <= timestamp) AND (timestamp <= %f)
-                      AND txid = %d
-                    ORDER BY timestamp ASC''' % (t_start, t_end, tx_id))
-    
-    pos = cur.fetchall()
-    
-    # Average.
-    mean_speed = 0;
-    for i in range(len(pos)-1): 
-      Pi = np.complex(pos[i][3], pos[i][4])
-      Pj = np.complex(pos[i+1][3], pos[i+1][4])
-      t_delta = float(pos[i+1][2]) - float(pos[i][2])
-      assert t_delta > 0
-      mean_speed += dist(Pi, Pj) / t_delta
-    mean_speed /= len(pos)
-
-    # NOTE This approach seems to be reasonable for one candidate per time 
-    #  unit. 
-    
-    # Standard deviation.
-    #stddev_speed = 0
-    #for i in range(len(pos)-1): 
-    #  Pi = np.complex(pos[i][3], pos[i][4])
-    #  Pj = np.complex(pos[i+1][3], pos[i+1][4])
-    #  t_delta = float(pos[i+1][2]) - float(pos[i][2])
-    #  assert t_delta > 0
-    #  stddev_speed += ((dist(Pi, Pj) / t_delta) - mean_speed) ** 2
-    #stddev_speed = np.sqrt(stddev_speed / len(pos))
-    #print (mean_speed, stddev_speed)
-    
-    tracks = [[ (np.complex(pos[0][3], pos[i][4]), float(pos[0][2])) ]]
-    for i in range(1, len(pos)): 
-      P_i = np.complex(pos[i][3], pos[i][4])
-      t_i = float(pos[i][2])
-      guy = False
-      for track in tracks:
-        (P_j, t_j) = track[-1]
-        assert (t_i - t_j > 0)
-        if dist(P_j, P_i) / (t_i - t_j) <= mean_speed/2:
-          track.append( (P_i, t_i) )
-          guy = True
-          break
-      if not guy: 
-        tracks.append( [(P_i, t_i)] )
-
-    print [ len(track) for track in tracks ]
-
-    m_track = None
-    m_size = 0
-    for track in tracks:
-      if len(track) > m_size:
-        m_track = track
-        m_size = len(track)
-
-    self.track = m_track
- 
-
-class track2:
-
-  ''' Track2.
 
   :param max_speed: Maximum foot speed of target (m/s). 
   :type max_speed: float
@@ -186,7 +125,8 @@ class track2:
       assert t_delta > 0 # TODO 
       mean_speed += dist(Pi, Pj) / t_delta
     mean_speed /= len(pos)
-
+    
+    # TODO make this a method. 
     # Build tracks DAG.
     nodes = []
     roots = []; leaves = []
@@ -204,8 +144,8 @@ class track2:
         for k in range(len(leaves)):
           if leaves[k].dist(node) / (node.t - leaves[k].t) < mean_speed: 
             ok = True
-            node.adj.append(leaves[k])
-            leaves[k].children.append(node)
+            node.adj_in.append(leaves[k])
+            leaves[k].adj_out.append(node)
         
         if not ok: # New root. 
           roots.append(node) 
@@ -216,38 +156,63 @@ class track2:
       # Recalculate leaves. 
       newLeaves = []
       for u in leaves:
-        if len(u.children) == 0: 
+        if len(u.adj_out) == 0: 
           newLeaves.append(u)
         else: 
-          for v in u.children:
+          for v in u.adj_out:
             if v not in newLeaves:
               newLeaves.append(v)
       leaves = newLeaves
 
       i = j
 
-    # I observed that the number of leaves is generally smaller than 
-    # the number of roots. Reverse the direction of the edges (using
-    # node.adj as the adjacency list) and find the path with the 
-    # highest likelihood. TODO  
-    for leaf in leaves: 
-      print leaf.dfs()
-      for node in nodes:
-        node.visited = False
+    self.track = self.critical_path(self.toposort(roots), C=-10)
+    self.track.reverse()
 
-    print len(roots)
-    print len(leaves)
-    self.track = []
 
-  def critical_path(self, s): 
-    # TODO Return most likely path, using ``ll``. 
-    # w(i -> j) = nodes[i].ll. Probably negate the edge 
-    # weights and do Bellman-Ford (http://en.wikipedia.org/wiki/
-    # Longest_path_problem#Acyclic_graphs_and_critical_paths).
-    return []
+  def visit(self, u, sorted_nodes): 
+    # TODO combine with toposort to do stack-style DFS. 
+    if u.t_visited and not u.t_sorted: 
+      raise "A cycle!"
+    elif not u.t_visited:
+      u.t_visited = True
+      for v in u.adj_out: 
+        self.visit(v, sorted_nodes)
+      u.t_sorted = True
+      sorted_nodes.append(u)
+    
+  def toposort(self, roots): 
+    sorted_nodes = [] 
+    for u in roots:
+      self.visit(u, sorted_nodes)
+    sorted_nodes.reverse()
+    return sorted_nodes
 
+  def critical_path(self, sorted_nodes, C): 
+    cost = 0
+    node = None 
+    for v in sorted_nodes: 
+      mdistance = 0
+      mparent = None
+      for u in v.adj_in:
+        if u.distance > mdistance:
+          mdistance = u.distance
+          mparent = u
+      v.parent = mparent
+      v.distance = mdistance + C + v.ll 
+      
+      if v.distance > cost:
+        cost = v.distance
+        node = v
+      
+    path = []
+    while node != None:
+      path.append((node.P, node.t))
+      node = node.parent
+    return path
     
 
+ 
 
 
 
@@ -260,11 +225,7 @@ if __name__ == '__main__':
 
   (t_start_feb2, t_end_feb2, tx_id_feb2) = (1391390700.638165, 1391396399.840252, 54)
 
-  fella = track2(db_con, t_start, t_end, tx_id, 4.4)
-  # With max_spaeed = 0.201, results are strange. 
-  # I expect see the largest component to be free of most 
-  # the false estimations. Instead I'm seeing a range of 
-  # values along the path missing. TODO
+  fella = track(db_con, t_start_feb2, t_end_feb2, tx_id_feb2, 4.4)
 
   import matplotlib.pyplot as pp
 
