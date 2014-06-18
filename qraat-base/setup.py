cd27d50ba3747f472feff345c5954a952d6e33c6
Christopher Patton <chrispatton@gmail.com>
2014-06-18 13:41:05 -0700

    Using distutils/Cmake for installation of Python code.

---
+++ qraat-base/setup.py
@@ -0,0 +1,9 @@
+
+from distutils.core import setup
+
+setup(name='QRAAT-base',
+  version='1.0',
+  description='QRAAT base Python package',
+  packages=['qraat'],
+  package_dir={'qraat' : 'python'},
+)
