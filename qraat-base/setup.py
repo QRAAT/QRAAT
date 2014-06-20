ca68c334cc22244bf6ea961508e0b5753b313667
Christopher Patton <chrispatton@gmail.com>
2014-06-20 14:42:36 -0700

    Fixed qraat-base/swig cludge.

--- qraat-base/setup.py
+++ qraat-base/setup.py
@@ -1,4 +1,3 @@
-
 from distutils.core import setup, Extension
 
 setup(name='QRAAT-base',
@@ -6,15 +5,8 @@ setup(name='QRAAT-base',
   description='QRAAT base Python package',
   packages=['qraat'],
   ext_modules=[Extension('qraat._pulse_data', 
-                         sources=['swig/pulse_data.i', 'lib/pulse_data.cc'],
+                         sources=['python/pulse_data.i', 'lib/pulse_data.cc'],
                          swig_opts = ['-c++'], 
                          include_dirs=['include'])],
-  py_modules=['qraat.pulse_data'],
   package_dir={'qraat' : 'python'},
 )
-
-# TODO swig produces necessary 'pulse_data.py' as a side-affect and leaves it 
-# in swig/. Figure out how to make this staging happen in build/, and modify 
-# swig/CMakeLists.txt appropriately. 
-# file python/pulse_data.py (for module qraat.pulse_data) not found
-# file python/pulse_data.py (for module qraat.pulse_data) not found
