bfb85a38d9f39180db2dd993cd6594ed6af5c00e
Christopher Patton <chrispatton@gmail.com>
2014-07-23 14:42:51 -0700

    Fixed namespace issues for template tags.

--- www/qraatview/qraatview/urls.py
+++ www/qraatview/qraatview/urls.py
@@ -6,60 +6,64 @@ from django.views.i18n import javascript_catalog
 
 admin.autodiscover()
 
-urlpatterns = patterns('qraatview.views',
-  url(r'^qraat_ui/', include('qraat_ui.urls', namespace="ui")),
-  url(r'^admin/', include(admin.site.urls)),
-  url(r'^auth/', include('qraat_auth.urls', namespace="auth")),
-  url(r'^jsi18n/$', javascript_catalog),
-    url(r'^$', 'index'),
-
+    
+project_patterns = patterns('qraatview.views',
     # url(r'^transmitters/$', 'transmitters'),
 
     # url(r'^transmitters/(?P<transmitter_id>\d+)/$',
     #     'get_transmitter'),
 
-    url(r'projects/$', 'projects', name='projects'),
+    url(r'^$', 'projects', name='projects'),
 
-    url(r'projects/create-project/$',
+    url(r'^create-project/$',
         'create_project', name='create-project'),
 
     url(
-        r'^project/(?P<project_id>\d+)/$', 'show_project',
+        r'^(?P<project_id>\d+)/$', 'show_project',
         name='show-project'),
 
     url(
-        r'^project/(?P<project_id>\d+)/transmitter/(?P<transmitter_id>\d+)/$',
+        r'^(?P<project_id>\d+)/transmitter/(?P<transmitter_id>\d+)/$',
         'show_transmitter',
         name='show-transmitter'),
 
     url(
-        r'^project/(?P<project_id>\d+)/location/(?P<location_id>\d+)/$',
+        r'^(?P<project_id>\d+)/location/(?P<location_id>\d+)/$',
         'show_location',
         name='show-location'),
 
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/$',
+        r'^(?P<project_id>\d+)/edit-project/$',
         'edit_project', name='edit-project'),
 
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/add-manufacturer/$',
+        r'^(?P<project_id>\d+)/edit-project/add-manufacturer/$',
         'add_manufacturer', name='add_manufacturer'),
     
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/add-location/$',
+        r'^(?P<project_id>\d+)/edit-project/add-location/$',
         'add_location', name='add-location'),
 
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/add-transmitter/$', 'add_transmitter',
+        r'^(?P<project_id>\d+)/edit-project/add-transmitter/$', 'add_transmitter',
         name='add-transmitter'),
 
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/add-target/$', 'add_target',
+        r'^(?P<project_id>\d+)/edit-project/add-target/$', 'add_target',
         name='add-target'),
 
     url(
-        r'^project/(?P<project_id>\d+)/edit-project/add-deployment/$', 'add_deployment',
-        name='add-deployment'),
+        r'^(?P<project_id>\d+)/edit-project/add-deployment/$', 'add_deployment',
+        name='add-deployment')
+)
 
-    url(r'^regular-content/$', 'regular_content'),
+
+urlpatterns = patterns('qraatview.views',
+  url(r'^qraat_ui/', include('qraat_ui.urls', namespace="ui")),
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^auth/', include('qraat_auth.urls', namespace="auth")),
+  url(r'^jsi18n/$', javascript_catalog),
+  url(r'^$', 'index'),
+  url(r'^regular-content/$', 'regular_content'),
+  url(r'^project/', include((project_patterns, 'qraat', 'qraatview')))
 )
