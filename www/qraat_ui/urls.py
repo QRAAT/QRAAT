462859c8c0bf329e63ba0816de3b7b06a7443f99
Jeymisson <jeymissoncz@gmail.com>
2014-07-25 17:06:28 -0700

    checking authorization for view-by-depid

--- www/qraat_ui/urls.py
+++ www/qraat_ui/urls.py
@@ -6,7 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('qraat_ui.views',
     url(r'^$', 'index'),
-    url(r'^deployment/(?P<dep_id>\d+)/$', 'view_by_dep'),
+    url(
+        r'^project/(?P<project_id>\d+)/deployment/(?P<dep_id>\d+)/$',
+        'view_by_dep', name='view_by_dep'),
+    # url(r'^deployment/(?P<dep_id>\d+)/$', 'view_by_dep'),
     url(r'^target/(?P<target_id>\d+)/$', 'view_by_target'),
     url(r'^transmitter/(?P<tx_id>\d+)/$', 'view_by_tx')
 )
