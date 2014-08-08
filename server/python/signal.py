129fc490669f91be44188bbd903edfef3cab4a1d
Christopher Patton <chrispatton@gmail.com>
2014-08-08 16:29:30 -0700

    Set SCORE_INTERVAL = 10 min.

--- server/python/signal.py
+++ server/python/signal.py
@@ -24,7 +24,7 @@ import MySQLdb as mdb
 # Some parameters. 
 BURST_INTERVAL = 10      # seconds
 BURST_THRESHOLD = 20     # pulses/second
-SCORE_INTERVAL = 60 * 15 # seconds
+SCORE_INTERVAL = 60 * 10 # seconds
 SCORE_OVERLAP = 60       # seconds
 SCORE_ERROR = 0.02       # seconds
 
