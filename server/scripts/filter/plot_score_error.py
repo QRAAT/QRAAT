ec3103387465f7a02071c8af717e252d459337e1
Christopher Patton <chrispatton@gmail.com>
2014-10-13 11:48:44 -0700

    Plot "optimal" false pos/neg trade-off.

--- server/scripts/filter/plot_score_error.py
+++ server/scripts/filter/plot_score_error.py
@@ -9,20 +9,34 @@ import pickle
 
 EST_SCORE_THRESHOLD = float(sys.argv[1])
 
-(x, y, pos, neg) = pickle.load(open('result%0.1f' % EST_SCORE_THRESHOLD))
-extent = [np.min(x), np.max(x), 
-          np.min(y), np.max(y)]
-#extent = [x[0], x[-1], y[0], y[-1]]
-
-#print x
-#print '-------------------'
-#print y
-#print '-------------------'
-#print pos
+(X, Y, pos, neg) = pickle.load(open('result%0.1f' % EST_SCORE_THRESHOLD))
+extent = [np.min(X), np.max(X), 
+          np.min(Y), np.max(Y)]
 
 
+pos_norm = float(np.max(pos.flat))
+neg_norm = float(np.max(neg.flat))
+
+C_p = 2 
+C_n = 1
+opt = []
+for i in range(len(X)):
+  min_score = float("+inf")
+  min_index = 0 
+  for j in range(len(Y)): 
+    score = (C_p * (pos[j,i] / pos_norm)) + (C_n * (neg[j,i] / neg_norm)) 
+    if score < min_score:
+      min_index = j
+      min_score = score
+  opt.append(Y[min_index])
+
+#X = np.array(list(reversed(X)))
+#opt = list(reversed(opt))
+
+# False positives
 pp.imshow(pos, extent=extent, aspect='auto', interpolation='nearest')
-pp.plot(x, qraat.srv.signal.SCORE_ERROR(x), 'k-')
+pp.plot(X, qraat.srv.signal.SCORE_ERROR(X), 'k-')
+pp.plot(X, opt, 'wo')
 fig = pp.gcf()
 fig.set_size_inches(16,12)
 cb = pp.colorbar()
@@ -33,8 +47,10 @@ pp.xlabel("Variation")
 pp.ylabel("Score error")
 pp.clf()
 
+# False negatives
 pp.imshow(neg, extent=extent, aspect='auto', interpolation='nearest')
-pp.plot(x, qraat.srv.signal.SCORE_ERROR(x), 'k-')
+pp.plot(X, qraat.srv.signal.SCORE_ERROR(X), 'k-')
+pp.plot(X, opt, 'wo')
 fig = pp.gcf()
 fig.set_size_inches(16,12)
 cb = pp.colorbar()
