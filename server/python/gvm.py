87539a45944939f3a1daeedb99c30e5f0a604143
Christopher Patton <chrispatton@gmail.com>
2015-01-05 16:53:42 -0800

    Probability distribution calculation is working.

--- server/python/gvm.py
+++ server/python/gvm.py
@@ -102,7 +102,7 @@ class Signal:
 
   def __init__(self, db_con, dep_id, t_start, t_end, score_threshold=0):
    
-    ''' Reprsent signals in the `qraat.est` table.
+    ''' Represent signals in the `qraat.est` table.
       
       Store data on a per site basis. Compute the bearing of each pulse 
       using Bartlet's estimator [ref] with respect to a set of steering 
@@ -139,10 +139,12 @@ class Signal:
 
       tnp = raw_data[:,12]
       noise_cov = np.zeros((raw_data.shape[0],position.num_ch,position.num_ch),dtype=np.complex)
-      for i in range(position.num_ch):
-        for j in range(position.num_ch):
-          noise_cov[:,i,j] = raw_data[:,2*j+13] + np.complex(0,-1)*raw_data[:,2*j+14]
-      
+      for t in range(raw_data.shape[0]):      
+        for i in range(position.num_ch):
+          for j in range(position.num_ch):
+            k = 13 + (i*position.num_ch*2) + (2*j)
+            noise_cov[t,i,j] = np.complex(raw_data[t,k], raw_data[t,k+1])
+
       for site_id in set(site_ids):
         site = _site_data(site_id)
         site.est_ids = est_ids[site_ids == site_id]
@@ -187,38 +189,25 @@ class _site_data:
   def __len__(self):
     return self.count
 
-  def prob(self, theta, sv): 
+  def prob(self, theta, i, sv): 
     ''' Probability of received signals given bearing `theta`. ''' 
     bearing = (180 / np.pi) * theta
     
-    print "bearing", bearing
-    V = np.matrix(self.signal_vector)
+    V = np.matrix(self.signal_vector[i])
     index = sv.bearings[self.site_id][bearing]
     G = np.matrix(sv.steering_vectors[self.site_id][index]).transpose()
 
-    R = np.zeros((self.count,4,4), dtype=np.complex)
-    guy = np.dot(G, np.conj(np.transpose(G)))
-    for i in range(R.shape[0]):
-      R[i] = self.tnp[i] * guy + self.noise_cov[i].reshape(4,4)
+    R = np.dot(G, np.conj(np.transpose(G))) + (self.noise_cov[i] / self.tnp[i])
+    det = np.abs(np.linalg.det(R))
     
-    det = np.linalg.det(R)
-   
-    p = np.zeros(self.count, dtype=np.complex)
-    for i in range(R.shape[0]):
-      p[i] = np.dot(np.conj(V[i]), np.dot(np.linalg.pinv(R[i]), np.transpose(V[i]))).flat[0]
-    
-    print p
-    
-    sys.exit(0)
-  
-    print G.shape
-    print self.tnp.shape
-    print self.noise_cov.shape
+    if det != 0: 
+      R = np.linalg.inv(R)
+      p = np.dot(np.transpose(np.conj(np.transpose(V))), 
+                     np.dot(R, np.transpose(V)))
+      p = np.exp(-np.abs(p.flat[0])) / (det * (np.pi ** position.num_ch))
+      return p
+    else: return 0.0
 
-
-    left_half = np.dot(V, np.conj(np.transpose(G)))
-    likelihood = np.real(left_half * np.conj(left_half))
-    return likelihood
 
   def bartlets_estimator(self, sv):
     ''' Compute the most likely bearing for each signal. '''
@@ -415,14 +404,14 @@ def test1():
   cal_id = 3
   dep_id = 105
   t_start = 1407452400 
-  t_end = 1407455985 #- (50 * 60)
+  t_end = 1407455985 - (50 * 60)
 
   db_con = util.get_db('reader')
   sv = position.steering_vectors(db_con, cal_id)
   signal = Signal(db_con, dep_id, t_start, t_end)
   
-  
-  print signal[2].prob(np.pi, sv)
+  for theta in np.arange(0,two_pi,two_pi/100): 
+    print signal[2].prob(theta, 22, sv)
 
 
 if __name__ == '__main__':
