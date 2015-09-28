8528dd04553cfc33b6da1b9cca5d6d69d81d7896
Peggy Li <peggyli1217@gmail.com>
2015-09-28 13:32:15 -0700

    Got rid of a print

--- www/map/forms.py
+++ www/map/forms.py
@@ -35,8 +35,6 @@ class Form(forms.Form):
   def __init__(self, deps=[], req_deps=[], data=None, label_suffix=''):
     #self.deployment_id = depID
     super(forms.Form, self).__init__(data, label_suffix='')
-    print "********dir(self)********"
-    print dir(self)
     self.fields['deployment'].choices = get_choices(deps)
     #self.fields['graph_dep'].choices = get_deps(req_deps)
     #Use get_choices(req_deps) if don't need to limit num of selected deps
